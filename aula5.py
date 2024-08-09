bc09ff28946bcac766f305f03ccc14d982d05ee7
leolac <leoalmcam@gmail.com>
2024-08-09 14:47:11 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,56 @@
+# 1
+print('Boas vindas!')
+
+# 2
+booleana = True
+print(type(booleana))
+
+# 3
+print(4.2 * 5.7)
+
+# 4
+print(10  / 5)
+
+# 5
+print(18 - 3)
+
+# 6
+print(30 / 10)
+
+# 7
+print(2 * 3 * 5 * 8)
+
+# 8
+numerointeiro =  8
+print(numerointeiro * 2)
+
+# 9
+soma1 = int(input('Insira o primeiro número a ser somado: '))
+soma2 = int(input('Insira o segundo número a ser somado: '))
+resultado = soma1 + soma2
+print('O resultado da soma é', resultado)
+
+# 10
+nomedeusuario = input('Insira um nome de usuário ')
+senha = input('Insira uma senha ')
+print('O seu nome de usuário é', nomedeusuario, 'e a sua senha é', senha)
+
+# Exercício 1
+umavinte = list(range(1, 21))
+print(umavinte)
+
+# Exercício 2
+cincoa50 = list((range(5, 51, 5)))
+print(cincoa50)
+print(sum(cincoa50))
+
+# Exercício 3
+variavel = False
+type(variavel)
+
+# Exercício 4
+print('Bem-vindo ao programa,', nomedeusuario)
+
+# Exercício 5
+impares = list(range(1, 11, 2))
+print(impares)
